afc159d1b9ad7150b8f195cdaa8fa83113f01a84
Evan Hu <exh4wrk@gmail.com>
2024-04-03 19:55:56 -0400

    copied OAuthData.java to backend-dokku

---
+++ src/main/java/edu/lehigh/cse216/exh226/backend/OAuthData.java
@@ -0,0 +1,11 @@
+package main.java.edu.lehigh.cse216.exh226.backend;
+
+/**
+ * OAuthData class:
+ * !! THIS CLASS IS ONLY MEANT FOR WORKING WITH THE JSON OBJECTS THAT GOOGLE'S
+ * OAUTH RETURNS !!
+ */
+public class OAuthData {
+    public String token_endpoint; // the Discovery Document's token_endpoint
+    public String id_token; // The OAuth id_token in the HTTP POST response
+}
